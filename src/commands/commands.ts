6c75b2373e8bd2ff4234aeb281f2e5769ff4b8b6
Prasad-D-Ware <prasadware42116@gmail.com>
2025-05-04 19:40:32 +0530

    ft : custom command for local sending

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,23 +1,42 @@
-import { REST, Routes } from 'discord.js';
+import { REST, Routes } from "discord.js";
 
 const commands = [
-    {
-        name : 'start',
-        description : "Creates a wallet or shows the exisiting wallet"
-    }
-  ];
-  
-  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!);
-  
-  export const registerCommands = async() =>{
-    try {
-      console.log('Started refreshing application (/) commands.');
-    
-      await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands });
-        // console.log(commands);
-      console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
+	{
+		name: "start",
+		description: "Creates a wallet or shows the exisiting wallet",
+	},
+	{
+		name: "send",
+		description: "send sol locally to discord members on solscope",
+		options: [
+			{
+				name: "user",
+				description: "Select a Discord user to send SOL to",
+				type: 6, // USER type
+				required: true,
+			},
+			{
+				name: "amount",
+				description: "Amount of SOL to send",
+				type: 3,
+				required: true,
+			},
+		],
+	},
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+
+export const registerCommands = async () => {
+	try {
+		console.log("Started refreshing application (/) commands.");
+
+		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+			body: commands,
+		});
+		// console.log(commands);
+		console.log("Successfully reloaded application (/) commands.");
+	} catch (error) {
+		console.error(error);
+	}
+};
